7714db240882e2ae719c315ef1bbcbba5e9f7eea
Tu Nombre <jmarloncin07@gmail.com>
2025-05-26 18:59:33 -0500

    Agregando la seccion principal del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import { Header } from './layout/Header'
 import { Title } from './components/Title'
-
+import { Intermediate } from './layout/Intermediate'
+import { Cards } from './layout/Cards'
 function App() {
 
   return (
     <div className='bg-[#393b38] font-open flex flex-col  min-h-screen w-full gap-16'>
       <Header />
-      <main className='flex-1'>
+      <main className='flex-1 flex flex-col gap-36'>
         <Title />
+        <Intermediate />
+        <Cards />
       </main>
     </div>
   )
