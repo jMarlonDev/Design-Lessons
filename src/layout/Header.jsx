573526534b85c336d82b9093dc338320cc3ed46a
Tu Nombre <jmarloncin07@gmail.com>
2025-05-26 10:24:52 -0500

    Creando los diseños reutilizando los componentes del sitio web

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,20 @@
-import logo from "../assets/Logo Oficial.webp"
+import logo from "../assets/LogoDev.png"
+import { Link } from "../components/Link"
+import { Button } from "../components/Button"
 export const Header = () => {
 
   return (
-    <img src={logo} alt="" />
+    <header className="flex justify-between items-center text-[17px] border-b-2 border-b-[#d2d2d0] p-5">
+      <div className="flex justify-center items-center gap-9">
+        <img src={logo} alt="Logo" className="w-24 h-auto rounded-4xl" />
+        <Link text={"inicio"} />
+        <Link text={"Sobre mí"} />
+      </div>
+
+      <div className="flex justify-center items-center gap-6">
+        <Link text={"Clases"} />
+        <Button textButton={"contacto"} />
+      </div>
+    </header>
   )
 }
