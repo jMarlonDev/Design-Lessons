5b8521bfd7d4f08628fa6efd06c6c04890be20a7
Tu Nombre <jmarloncin07@gmail.com>
2025-05-27 14:46:44 -0500

    Modificación: Titulo principal de la pagina mejorado

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,12 @@
 
 export const Title = () => {
   return (
-    <section className="relative flex justify-center items-center gap-28">
-      <div className="relative">
-        <h1 className="text-[#d2d2d0] leading-[1] text-[130px] font-bold uppercase text-center relative z-20">
-        <p className="absolute left-3 -top-[40px] w-[200px] h-[200px] z-10 text-[#ff5102]">#</p>
+    <section className="relative flex justify-center items-center text-center gap-28 py-10">
+      <div className="relative max-w-4xl w-full">
+        <h1 className="text-[#d2d2d0] leading-tight text-[80px] font-bold uppercase text-center relative z-20 px-8">
+          <p className="absolute -left-9 -top-8 w-40 h-96 z-10 text-[#ff5102]">#</p>
           mejora tu diseño con nuestra clase de diseño
-        <p className="absolute right-72 top-64 w-[200px] h-[200px] z-10 text-[#ff5102]">!</p>
+          <p className="absolute right-16 bottom-0 w-16 h-16 z-10 text-[#ff5102]">!</p>
         </h1>
       </div>
     </section>
