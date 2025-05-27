75581b98f172a4a06d239539c6c8005dc1c06335
Tu Nombre <jmarloncin07@gmail.com>
2025-05-27 14:47:33 -0500

    Modificación: Organización del contenido y padding del header principal

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,12 @@
-import logo from "../assets/LogoDev.png"
+import logo from "../assets/Logo Oficial.webp"
 import { Link } from "../components/Link"
 import { Button } from "../components/Button"
 export const Header = () => {
 
   return (
-    <header className="flex justify-between items-center text-[17px] border-b-2 border-b-[#d2d2d0] p-5">
+    <header className="flex justify-between items-center text-[17px] border-b-2 border-b-[#d2d2d0]/21 px-3.5">
       <div className="flex justify-center items-center gap-9">
-        <img src={logo} alt="Logo" className="w-24 h-auto rounded-4xl" />
+        <img src={logo} alt="Logo" className="w-[80px]  object-contain align-middle" />
         <Link text={"inicio"} />
         <Link text={"Sobre mí"} />
       </div>
